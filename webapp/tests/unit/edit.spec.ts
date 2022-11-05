c6e75ab4a02fc7278aee215b176324faac34f48b
Filip Lindqvist <buggfille@gmail.com>
2022-11-05 10:35:12 +0100

    Fix tests

--- webapp/tests/unit/edit.spec.ts
+++ webapp/tests/unit/edit.spec.ts
@@ -2,13 +2,31 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import RecipeEdit from '@/views/RecipeEdit.vue'
 
 import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
+localVue.use(VueRouter)
+
+jest.mock('vue-router/composables', () => ({
+  useRoute: () => ({
+    params: { key: '' },
+  }),
+  useRouter: () => ({ push: () => null })
+}));
+
+jest.mock('firebase/firestore', () => ({
+  getFirestore: () => {},
+  doc: () => {},
+  onSnapshot: () => {},
+  getDoc: () => Promise.resolve({ exists: () => true, data: () => ({}) })
+}));
 
 describe('RecipeEdit.vue', () => {
   it('renders text', () => {
-    const wrapper = shallowMount(RecipeEdit, { localVue })
+    const wrapper = shallowMount(RecipeEdit, {
+      localVue,
+    })
     expect(wrapper.text()).toContain('Nytt Recept')
   })
 })
