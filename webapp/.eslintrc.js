e4a1adec43e52ebeaf0815506826c8740e54926e
Filip Lindqvist <buggfille@gmail.com>
2024-02-20 20:48:57 +0100

    Add eslint

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
   root: true,
-
   env: {
     node: true
   },
-
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
-
   parserOptions: {
     ecmaVersion: 2020
   },
-
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
-    '@typescript-eslint/camelcase': ['error', { properties: 'never', ignoreDestructuring: true }]
-  },
+    'vue/no-unused-components': 'warn',
+    'no-use-before-define': 'warn',
+    camelcase: 'warn',
+    'vue/multi-word-component-names': 'off',
+    'vue/max-attributes-per-line': ['error', {
 
+    }],
+    '@typescript-eslint/no-unused-vars': 'off'
+  },
   overrides: [
     {
       files: [
+        '**/__tests__/*.{j,t}s?(x)',
         '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
