337734b07b036af1bf3b75b84b2cf0c3ea62da2a
Filip Lindqvist <buggfille@gmail.com>
2024-11-26 09:35:07 +0100

    Update Units.vue

--- webapp/src/views/Units.vue
+++ webapp/src/views/Units.vue
@@ -1,113 +1,34 @@
 <template>
-  <b-container :class="{'converts': true, 'disabled': !user }">
-    <h1>Måttenheter</h1>
-    <b-row
-      v-if="data"
-      class="clearfix"
-    >
-      <b-col
-        v-for="[key, item] in sortBy(Object.entries(data), ([k,]) => k)"
-        :key="key"
-        cols="12"
-      >
-        <b-row
-          no-gutters
-          class="mb-2 mt-2"
-        >
-          <b-col cols="10">
-            <b-form-input
-              size="lg"
-              :value="item.name"
-              :state="isValidPattern(item.name)"
-              @input="update(key, 'name', $event)"
-            />
-          </b-col>
-          <b-col
-            cols="auto"
-            class="item icon trash"
-            @click="remove(key)"
-          >
-            <b-icon-trash-fill
-              scale="1.4"
-              class=""
-            />
-          </b-col>
-        </b-row>
-        <div
-          v-for="([subkey, line], idx) in sortBy(Object.entries(item.lines), ([k,]) => k)"
-          :key="key+subkey"
-        >
-          <b-row
-            no-gutters
-            class=""
-          >
-            <b-col
-              v-if="idx === Object.keys(item.lines).length - 1"
-              cols="auto"
-              class="line icon plus show"
-              @click="addLine(key)"
-            >
-              <b-icon-plus-circle-fill
-                scale="1.4"
-                class=""
-              />
-            </b-col>
-            <b-col
-              v-else
-              cols="auto"
-              class="line icon plus"
-            >
-              <b-icon-plus-circle-fill
-                scale="1.4"
-                class=""
-              />
+  <b-container :class="{ 'converts': true, 'disabled': !user }">
+    <h1>Måttenheter
+
+      <small>{{ Object.keys(data).length }} st</small>
+
+    </h1>
+    <b-row v-if="data" class="clearfix">
+      <b-col v-for="[key, items] in sortBy(Object.entries(data), ([k,]) => k)" :key="key" cols="4" class="mt-4">
+        <b-form-input size="lg" :value="key" />
+
+        <div v-for="({grams, unit}) in sortBy(items, i => i.unit)" :key="key + unit" class="mt-2">
+          <b-row no-gutters class="">
+            <b-col cols="3">
+              <b-form-input :value="unit" />
             </b-col>
             <b-col cols="3">
-              <b-form-input
-                :value="line.measure"
-                :state="isValidPattern(line.measure)"
-                @change="updateLine(key, subkey, 'measure', $event )"
-              />
-            </b-col>
-            <b-col cols="3">
-              <b-form-input
-                :value="line.weight"
-                type="number"
-                @change="updateLine(key, subkey, 'weight', $event )"
-              />
-            </b-col>
-            <b-col cols="3">
-              <b-form-input
-                :value="line.weight_measure"
-                placeholder="g"
-                @change="updateLine(key, subkey, 'weight_measure', $event )"
-              />
-            </b-col>
-            <b-col
-              cols="auto"
-              class="line icon trash show"
-              @click="removeLine(key, subkey)"
-            >
-              <b-icon-trash-fill
-                scale="1.4"
-                class=""
-              />
+              <b-form-input :value="grams" placeholder="g" type="number" />
             </b-col>
           </b-row>
         </div>
       </b-col>
-      <b-col
-        cols="12"
-        class="mt-4"
-      >
-        <b-button
-          variant="primary"
-          :disabled="!user"
-          @click="add"
-        >
-          <b-icon-plus />
-          Lägg till
-        </b-button>
+    </b-row>
+    <hr/>
+    <b-row>
+      <b-col>
+        <b-form>
+          <b-form-group label="JSON">
+            <b-textarea v-model="manual" debounce="500" max-rows="5000" style="font-size: 0.75rem;"/>
+          </b-form-group>
+        </b-form>
       </b-col>
     </b-row>
   </b-container>
@@ -115,68 +36,49 @@
 
 <script lang="ts">
 import { useAuth } from '@/modules/use/auth'
-import { Converts } from '@/modules/use/auto-convert'
-import { doc, getFirestore, onSnapshot, deleteField, updateDoc, arrayUnion, arrayRemove } from '@firebase/firestore'
-import { uuidv4 } from '@firebase/util'
+import { Weights } from '@/modules/use/auto-convert'
+import { doc, getFirestore, onSnapshot } from '@firebase/firestore'
+import { debouncedWatch } from '@vueuse/core'
+import { setDoc } from 'firebase/firestore'
 import { sortBy } from 'lodash'
-import { defineComponent, reactive, ref, watch } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 
 const Component = defineComponent({
   components: {
   },
-  setup () {
+  setup() {
     const store = getFirestore()
     const { user } = useAuth()
 
-    const data = ref<Converts>({})
-    const weightsRef = doc(store, 'veganflora', 'root', 'converts', 'weight')
+    const data = ref<Weights>({})
+
+    const inputError = ref()
+    const manual = computed<string>({
+      get: () => JSON.stringify(data.value, null, 2),
+      set: (s: string) => {
+        try { data.value = JSON.parse(s); inputError.value = null } catch (e) { inputError.value = e }
+      }
+    })
+
+    const weightsRef = doc(store, 'veganflora', 'root', 'converts', 'weight-v2')
     onSnapshot(weightsRef, result => {
       if (result.exists()) {
         data.value = result.data()
       }
     })
 
-    function updateLine (key: string, subkey: string, attr: string, value: string | number) {
-      if (!user.value) return
-      updateDoc(weightsRef, { [`${key}.lines.${subkey}.${attr}`]: (typeof value === 'string' ? value.trim().toLowerCase() : value) })
-    }
-    function addLine (key: string) {
-      if (!user.value) return
-      updateDoc(weightsRef, { [`${key}.lines.${uuidv4()}`]: { measure: 'dl', weight: 0 } })
-    }
-    function removeLine (key: string, subkey: string) {
-      if (!user.value) return
-      updateDoc(weightsRef, { [`${key}.lines.${subkey}`]: deleteField() })
-    }
+    debouncedWatch(data, async (x, prev) => {
+      if (Object.keys(prev).length === 0) return
 
-    function update (key: string, field: 'name', value: string) {
-      if (!user.value) return
-      updateDoc(weightsRef, { [`${key}.${field}`]: value.trim().toLowerCase() })
-    }
-
-    function add () {
-      if (!user.value) return
-      const v: Converts = { [uuidv4()]: { name: '', lines: { [uuidv4()]: { measure: 'dl', weight: 0 } } } }
-      updateDoc(weightsRef, v)
-    }
-    function remove (key: string) {
-      if (!user.value) return
-      updateDoc(weightsRef, { [key]: deleteField() })
-    }
-    function isValidPattern (name?: string) {
-      return (name ?? '').trim().length > 0
-    }
+      console.log('Saving document')
+      await setDoc(weightsRef, x)
+    }, { maxWait: 5000, debounce: 1000 })
 
     return {
       data,
-      addLine,
-      removeLine,
-      updateLine,
-      update,
-      add,
-      remove,
+      manual,
+      inputError,
       sortBy,
-      isValidPattern,
       user
     }
   }
@@ -186,23 +88,27 @@ export default Component
 </script>
 
 <style scoped>
-
 .plus svg {
   color: var(--primary);
   margin-right: 12px;
 }
+
 .trash svg {
   color: var(--danger);
 }
+
 .icon {
   transition: all 100ms cubic-bezier(0.55, 0.055, 0.675, 0.19);
 }
+
 .icon:hover svg {
   scale: 1.2;
 }
+
 .icon:hover.show {
   cursor: pointer;
 }
+
 .icon:active svg {
   scale: 1.4;
 }
@@ -212,6 +118,7 @@ export default Component
   margin-top: 12px;
   margin-left: 14px;
 }
+
 .line.icon.show svg {
   visibility: visible;
 }
@@ -225,5 +132,4 @@ export default Component
 .converts.disabled .icon {
   opacity: 0.4;
 }
-
 </style>
