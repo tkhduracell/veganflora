fd059af452450bde7acb5fcde93768173138f61f
Filip Lindqvist <buggfille@gmail.com>
2024-11-26 09:34:35 +0100

    Update auto-convert.ts

--- webapp/src/modules/use/auto-convert.ts
+++ webapp/src/modules/use/auto-convert.ts
@@ -7,20 +7,22 @@ import { doc, getFirestore, onSnapshot } from 'firebase/firestore'
 import { Ingredient } from '@/components/types'
 import { normalizeValue } from '../ingredients'
 
-export type ConvertLine = { measure: string, weight: number, weight_measure?: string }
-export type Convert = { name: string, lines: { [key :string]: ConvertLine } }
-export type Converts = { [key :string]: Convert }
+export interface Weights {
+  [ingredient: string]: { unit: string, grams: number }[]
+}
 
 export function useAutoConvert () {
   const store = getFirestore()
-  const enabled = useLocalStorage('convert-weight', false)
-  const weights = ref<Converts>({})
+  const enabled = useLocalStorage('convert-weight-v2', false)
+  const weights = ref<Weights>({})
+
   const listeners = reactive({
     // @eslint-disable-next-line @typescript-eslint/no-empty-function
     weights: () => { /** */ }
   })
+
   onMounted(async () => {
-    const ref = doc(store, 'veganflora', 'root', 'converts', 'weight')
+    const ref = doc(store, 'veganflora', 'root', 'converts', 'weight-v2')
     listeners.weights = onSnapshot(ref, result => {
       weights.value = result.data() as UnwrapRef<typeof weights> ?? {}
     })
@@ -33,16 +35,13 @@ export function useAutoConvert () {
     const tbl = weights.value
     if (Object.keys(tbl).length === 0) return ingredient
     const norml = (s?: string) => (s ?? '').toLocaleLowerCase().trim()
-    for (const { lines, name } of Object.values(tbl)) {
-      // Prefer exact match
-      if (norml(name) === norml(ingredient.name)) {
-        for (const { measure, weight, weight_measure } of Object.values(lines)) {
-          // Prefer exact match
-          if (norml(measure) === norml(ingredient.measure)) {
-            return { ...ingredient, amount: tryMultiply(weight, ingredient.amount), measure: weight_measure ?? 'g' }
-          }
-        }
-      }
+
+    const grams = weights.value[norml(ingredient.name)]
+      .find(units => units.unit === ingredient.measure)
+      ?.grams ?? null
+
+    if (grams !== null) {
+      return { ...ingredient, amount: tryMultiply(grams, ingredient.amount), measure: 'g' }
     }
     return ingredient
   }
