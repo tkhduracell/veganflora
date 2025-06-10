2505040f454d6833dc4de26a3706909ee884b0be
Filip Lindqvist <buggfille@gmail.com>
2025-06-10 08:05:45 +0200

    fix: improve recipe summarization instructions and update model version

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -34,19 +34,25 @@ async function summarizeWithChatLLM(text: string): Promise<string> {
        You are a helpful AI assistant that can summarize recipes in Swedish
        and provide them in JSON format. 
        
-       Instructions: 
+       Instructions for the recipe text: 
         * Do not include ingredients used only for serving or granish in the ingredient list, mearly mention them in 
        the text at the last step. 
-        * The step by step text instruction should contain a list of steps in a Markdown language format.
-        * The step by step text instruction should not include any main header, but can contain several 
-          Markdown sections if there are natual parts to the recepie text instructions itself.
+        * Create a clear step by step text instruction, that should contain a list of steps in a bullet point format without any header.
+        * The text should not include any main header, and ONLY contain several Markdown sections if there are natual parts to the recepie text instructions itself.
         * Do not include "vegan" in the title or ingredients, since everything is assumed to be vegan.
+		* The title should be in Swedish, and should not include any brand names.
+		* The title should be a short and descriptive name for the recipe, e.g. "Lasagne" or "Chokladbollar".
+		* The text should not include any unit measurements, e.g. "1 dl mjölk" should be "mjölk".
+		* The text should not include any brand names, e.g. "Arla Ko® Standardmjöl" should be "mjölk".
+	   Instructions for the ingredient list:
+		* The ingredient list should be in Swedish, and use Swedish units (e.g. dl, tsk, msk, gram).
+	   	* Remove any brands from the ingredient list, e.g. "Arla Ko® Standardmjöl" should be "mjölk".
     `.replace(/\n/g, "");
 	const USER_PROMPT = `
       Summarize this recipe in Swedish with Swedish units: ${text}
     `.trim();
 	const response = await openai.chat.completions.create({
-		model: "gemini-2.0-flash",
+		model: "gemini-2.5-flash-preview-05-20",
 		messages: [
 			{ role: "system", content: [{ text: SYSTEM_PROMPT, type: "text" }] },
 			{ role: "user", content: [{ text: USER_PROMPT, type: "text" }] },
